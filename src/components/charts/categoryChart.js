bcf91c0ffa33210058d627643be27957a462792e
kozawjyee <kozawgyi551997@gmail.com>
2022-02-22 19:19:53 +0630

    [colorgenerator]

--- src/components/charts/categoryChart.js
+++ src/components/charts/categoryChart.js
@@ -4,6 +4,7 @@ import { Card } from "react-bootstrap";
 import { Doughnut } from 'react-chartjs-2';
 import { BsFillPieChartFill } from "react-icons/bs";
 import { t, zawgyi } from '../../utilities/translation.utility';
+import { colorGen, generateColor } from '../../utilities/colorgenerator'
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -31,40 +32,34 @@ export const CategoryChartComponent = ({ props, dataSource }) => {
                 const dataCount = dataSource.filter((item => item.category_title === catLabel)).length;
                 dataSet.push(dataCount);
             });
-            let value = [{r: 0, g: 0, b: 0, a: 1}];
-            for(let i = 1; i<=dataSet.length; i++){
-                let x = 255 / dataSet.length;
-                let intvalue = Math.floor(x);
-                let red = i;
-                let green = intvalue*i;
-                let blue = 255-green;
-                value.push({
-                    r: red,
-                    g: green,
-                    b: blue,
-                    a: 1
-                })
-            }
-            let rgba = [];
-            rgba.push(value.map((e, i) => `rgba(${e.r}, ${e.g}, ${e.b}, ${e.a})`));
-            console.log(rgba);
-
+            // let value = [{r: 0, g: 0, b: 0, a: 1}];
+            // for(let i = 1; i<=dataSet.length; i++){
+            //     let x = 255 / dataSet.length;
+            //     let intvalue = Math.floor(x);
+            //     let red = i;
+            //     let green = intvalue*i;
+            //     let blue = 255-green;
+            //     value.push({
+            //         r: red,
+            //         g: green,
+            //         b: blue,
+            //         a: 1
+            //     })
+            // }
+            // let rgba = [];
+            // rgba.push(value.map((e, i) => `rgba(${e.r}, ${e.g}, ${e.b}, ${e.a})`));
+            // let datalength = dataSet.length;
+            // let color = [];
+            // let getcolor = colorGen(datalength);
+            // color.push(chartColor(datalength));
+            
             const data = {
                 labels: uniqueLabel.length > 0 ? uniqueLabel : [],
                 datasets: [
                     {
                         label: "# of item categories",
                         data: dataSet,
-                        backgroundColor: rgba[0],
-                        borderWidth: 1,
-                    },
-                    {
-                        label: "# of item categorie",
-                        data: dataSet,
-                        backgroundColor: [
-                            'rgba(255, 100, 132, 1)',
-                            'rgba(54, 100, 235, 1)',
-                        ],
+                        backgroundColor: generateColor(uniqueLabel),
                         borderWidth: 1,
                     }
                 ]
