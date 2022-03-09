37da1ed174aea91b781ac44e9d8b0ee226db0659
aunghtetpaing <aunghp@hanamicrofinance.com>
2022-03-09 17:19:18 +0630

    [ADD] - sall

--- src/pages/Sale.js
+++ src/pages/Sale.js
@@ -1,7 +1,8 @@
-import { set } from "lodash";
+import { remove, set } from "lodash";
+import moment from "moment";
 import numeral from "numeral";
 import React, { Component} from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, FormControl, InputGroup } from "react-bootstrap";
 import { BsTrash } from "react-icons/bs";
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
@@ -16,112 +17,251 @@ class SalePage extends Component {
     constructor(props){
         super(props);
         this.state = {
-            items: [],
-            categories: [],
-            filter: 'All',
-            loading: true,
-            preCart: [],
-            totalAmount: 0,
-            removeItem: null
+            invoice_id: moment().unix(),
+            customerName: '',
+            customerAddress: '',
+            customerSearch: '',
+            name: '',
+            model: '',
+            code: '',
+            qty: 0,
+            cartItems: [],
+            total: 0
         };
     };
 
     async loadingData() {
-        const { openToast } = this.props.reducer;
+    }
 
-        const categories = await getCategories();
+    autoSearch(e) {
+        this.setState({
+            customerSearch: e
+        });
+    }
 
-        if(categories && categories.success === false) {
-            openToast(t('toast-sell'), categories.message, 'danger');
-            this.setState({
-                loading: false
-            })
-            return;
-        }
+    removeItem(item) {
+        const { cartItems } = this.state;
 
-        let mergeItems = [];
+        let updateItems = cartItems.filter((value) => {
+            if(value.code !== item.code || value.model !== item.model) {
+                return value;
+            }
+        });
 
-        mergeItems = categories.flatMap((category) => {
-            return category.items;
+        let total = 0;
+
+        updateItems.map((item) => {
+            total =+ item.total + total;
         });
 
         this.setState({
-            items: mergeItems,
-            categories: categories,
-            loading: false
+            cartItems: updateItems,
+            total: total
+        });
+        
+        return;
+    }
+
+    itemSearch(e, type) {
+    }
+
+    addCart() {
+        const {name, model, code, qty, cartItems } = this.state;
+        if(name === '' || model === '' || code === '' || qty === '') {
+            return;
+        }
+        
+        const price = 3000;
+
+        const item = {
+            name: name,
+            model: model,
+            code: code,
+            qty: qty,
+            price: price,
+            total: Number(qty) * Number(price)
+        }
+
+        let addItem = cartItems;
+
+        addItem.push(item);
+
+        let total = 0;
+
+        addItem.map((item) => {
+            total =+ item.total + total;
+        });
+
+        this.setState({
+            cartItem: addItem,
+            total: total
         });
     }
+    
 
     async componentDidMount(){
         await this.loadingData();
     }
 
-    getPreCart(e) {
-        this.setState({
-            preCart: e
-        });
-    }
-
-    payNow() {
-
-    }
     render(){
-        const { items, categories, preCart, totalAmount } = this.state;
+        const { customerSearch, customerName, customerPhone, customerAddress, name, model, code, qty, cartItems, total } = this.state;
         return(
             <>
                 <Navigation props={this.props} />
 
                 <div className="container-fluid">
-                    <div className="row mt-3">
-                        <div className="col-md-4">
-                            <Card>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Card className="mt-3">
                                 <Card.Header>
-                                   <Card.Title> {t('sell-cart')} </Card.Title>
+                                    <Card.Title className="title">
+                                        <div className="d-md-flex flex-md-row justify-content-between align-item-center">
+                                            <span>  INVOICE  </span>
+                                            <InputGroup className="card-header-search">
+                                                <FormControl 
+                                                    type="text"
+                                                    placeholder="Search Customer By Name"
+                                                    value={customerSearch}
+                                                    onChange={(e) => this.autoSearch(e.target.value)}
+                                                />
+                                                <Button className="btn btn-samll"> Choose Customer </Button>
+                                            </InputGroup>
+                                        </div>
+                                    </Card.Title>
                                 </Card.Header>
 
                                 <Card.Body>
-                                   <table>
-                                       <thead className="mb-3">
-                                       <tr>
-                                           <th className="cart-table-with"> Name </th>
-                                           <th className="cart-table-with"> Model </th>
-                                           <th className="cart-table-with"> Qty </th>
-                                           <th className="cart-table-with"> Price </th>
-                                           <th className="cart-table-with"> Total </th>
-                                           <th className="cart-table-with"> Remove </th>
-                                       </tr>
-                                       </thead>
+                                    <div className="d-md-flex flex-column mb-3">
+                                        <h3 className="mb-3"> INVOICE </h3>
 
-                                        <tbody className="mt-3">
-                                        {preCart.length > 0 && preCart.map((item, index) => {
-                                            return(
-                                                <tr key={`cart_item_ids_${index}`}>
-                                                    <td className="cart-table-with"> {item.name}</td>
-                                                    <td className="cart-table-with"> {item.model} </td>
-                                                    <td className="cart-table-with"> {item.qty} </td>
-                                                    <td className="cart-table-with"> {numeral(item.price).format('0,0')} </td> 
-                                                    <td className="cart-table-with"> {numeral(item.total).format('0,0')} </td>
-                                                    <td>
-                                                        <BsTrash size={20} className="btn-icon" onClick={() => this.setState({
-                                                            removeItem: item
-                                                        })} />
+                                        <InputGroup>
+                                            <FormControl 
+                                                className="me-3"
+                                                type="text"
+                                                placeholder="Enter Customer Name"
+                                                value={customerName}
+                                                onChange={(e) => this.setState({
+                                                    customerName: e.target.value
+                                                })}
+                                            />
+
+                                            <FormControl 
+                                                className="me-3"
+                                                type="text"
+                                                placeholder="Enter Customer Phone Number"
+                                                value={customerPhone}
+                                                onChange={(e) => this.setState({
+                                                    customerPhone: e.target.value
+                                                })}
+                                            />
+                                        </InputGroup>
+
+                                        <InputGroup className="mt-3">
+                                            <FormControl
+                                                as={'textarea'}
+                                                placeholder="Enter Customer Address"
+                                                value={customerAddress}
+                                                onChange={(e) => this.setState({
+                                                    customerAddress: e.target.value
+                                                })}
+                                            />
+                                        </InputGroup>
+                                    </div>
+
+                                    <div className="table-responsive">
+                                        <table className="table">
+                                            <thead>
+                                                <tr>
+                                                    <th className="cart-item-table-hash-width"> # </th>
+                                                    <th className="cart-item-table-with"> Name </th>
+                                                    <th className="cart-item-table-with"> Model </th>
+                                                    <th className="cart-item-table-with"> Material Code </th>
+                                                    <th className="cart-item-table-with"> Qty </th>
+                                                    <th className="cart-item-table-with"> Price </th>
+                                                    <th className="cart-item-table-with"> Total </th>
+                                                </tr>
+                                            </thead>
+
+                                            <tbody>
+                                                {cartItems.length > 0 && cartItems.map((item, index) => {
+                                                    return(
+                                                        <tr key={`cart_item_id_${index}`}>
+                                                            <td className="cart-item-table-hash-width"> {index + 1} </td>
+                                                            <td className="cart-item-table-with"> {item.name} </td> 
+                                                            <td className="cart-item-table-with"> {item.model} </td>
+                                                            <td className="cart-item-table-with"> {item.code} </td>
+                                                            <td className="cart-item-table-with"> {item.qty} </td>
+                                                            <td className="cart-item-table-with"> {numeral(item.price).format('0,0')} </td>
+                                                            <td className="cart-item-table-with"> 
+                                                                <span className="me-3"> {numeral(item.total).format('0,0')}  </span>
+                                                                <BsTrash className="btn-icon" size={20} onClick={() => this.removeItem(item)} />
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                                })}
+                                                <tr>
+                                                    <td className="cart-item-table-hash-width"> # </td>
+                                                    <td className="cart-item-table-with"> 
+                                                        <InputGroup>
+                                                            <FormControl 
+                                                                type="text"
+                                                                placeholder="Search Item Name"
+                                                                value={name}
+                                                                onChange={(e) => this.setState({ name: e.target.value})}
+                                                            />
+                                                        </InputGroup>
+                                                    </td>
+
+                                                    <td className="cart-item-table-with"> 
+                                                        <InputGroup>
+                                                            <FormControl 
+                                                                type="text"
+                                                                placeholder="Search Item Model"
+                                                                value={model}
+                                                                onChange={(e) => this.setState({ model: e.target.value})}
+                                                            />
+                                                        </InputGroup>
+                                                    </td>
+
+                                                    <td className="cart-item-table-with"> 
+                                                        <InputGroup>
+                                                            <FormControl 
+                                                                type="text"
+                                                                placeholder="Search Item code"
+                                                                value={code}
+                                                                onChange={(e) => this.setState({ code: e.target.value})}
+                                                            />
+                                                        </InputGroup>
+                                                    </td>
+
+                                                    <td className="cart-item-table-with"> 
+                                                        <InputGroup>
+                                                            <FormControl 
+                                                                type="text"
+                                                                placeholder="Qty"
+                                                                value={qty}
+                                                                onChange={(e) => this.setState({ qty: e.target.value})}
+                                                            />
+                                                        </InputGroup>
+                                                    </td>
+
+                                                    <td colSpan="2" className="cart-item-table-with">
+                                                        <Button 
+                                                            className="btn btn-small"
+                                                            onClick={() => this.addCart()}
+                                                        > Add Item 
+                                                        </Button>
                                                     </td>
                                                 </tr>
-                                            )
-                                        })}
-                                        </tbody>
-                                   </table>
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </Card.Body>
 
                                 <Card.Footer>
-                                    <Button className="btn btn-small" onClick={() => this.payNow()}> Pay Now </Button>
+                                    <h3> Total - {numeral(total).format('0,0')} </h3>
                                 </Card.Footer>
                             </Card>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <SaleItemListTableComponent props={this.props} items={items} categories={categories} preCart={(e) => this.getPreCart(e)} remove={this.state.removeItem} />
                         </div>
                     </div>
                 </div>
