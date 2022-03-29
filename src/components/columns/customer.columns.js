904ab452f0f3a4618c5db2de2bceba857a679906
kozawjyee <kozawgyi551997@gmail.com>
2022-03-29 23:09:26 +0630

    [sarchName&code]

--- src/components/columns/customer.columns.js
+++ src/components/columns/customer.columns.js
@@ -1,66 +1,68 @@
-import React from 'react'
-import { BsArrowUpRightSquare } from 'react-icons/bs';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
+import {BsArrowUpRightSquare} from 'react-icons/bs';
+import {useHistory} from 'react-router-dom';
 
-export const customerColumns = (props) => {
+export const customerColumns = props => {
+	const history = useHistory();
 
-  const history = useHistory()
-  
-  const columns = [
-    {
-      name: <span className='datatable-header'>#</span>,
-      selector: (row, index) => index + 1,
-      sortable: true,
-      width: '50px'
-    },
-    {
-      name: <span className='database-header'>Name</span>,
-      selector: row => row.customer_name,
-      sortable: true,
-    },
-    {
-      name: <span className='database-header'>Email</span>,
-      selector: row => row.customer_email,
-      sortable: true,
-    },
-    {
-      name: <span className='database-header'>Phone Number</span>,
-      selector: row => row.customer_phone,
-      sortable: true,
-    },
-    {
-      name: <span className='database-header'>Credit</span>,
-      selector: row => row.credit_amount,
-      sortable: true,
-    },
-    {
-      name: <span className='database-header'>Paid Count</span>,
-      selector: row => {
-        let getcredit = row.credit;
-        let repayment = JSON.parse(getcredit.repayment);
-        return repayment.length;
-      },
-      sortable: true,
-    },
-    {
-      name: <span className='database-header'>Total Amount</span>,
-      selector: row => row.total_amount,
-      sortable: true,
-      width: '175px'
-    },
-    // {
-    //   name: <span className='database-header'>Option</span>,
-    //   selector: (row) => {
-    //     return (
-    //       <>
-    //         <BsArrowUpRightSquare 
-    //         size={20}
-    //         className="icon-btn-outline"
-    //         onClick={() => history.push(`/editcustomer/${row.id}`)}/>
-    //       </>
-    //     )
-    //   }
-    // }
-  ];
-  return columns;
-}
+	const columns = [
+		{
+			name: <span className="datatable-header">#</span>,
+			selector: (row, index) => index + 1,
+			sortable: true,
+			width: '50px'
+		},
+		{
+			name: <span className="database-header">Name</span>,
+			selector: row => row.customer_name,
+			sortable: true
+		},
+		{
+			name: <span className="database-header">Email</span>,
+			selector: row => row.customer_email,
+			sortable: true
+		},
+		{
+			name: <span className="database-header">Phone Number</span>,
+			selector: row => row.customer_phone,
+			sortable: true
+		},
+		{
+			name: <span className="database-header">Credit</span>,
+			selector: row => row.credit_amount,
+			sortable: true
+		},
+		{
+			name: <span className="database-header">Paid Count</span>,
+			selector: row => {
+				if (row.credit !== null) {
+					let getcredit = row.credit;
+					let repayment = JSON.parse(getcredit.repayment);
+					return repayment.length;
+				}
+				return (row = 1);
+			},
+			sortable: true
+		},
+		{
+			name: <span className="database-header">Total Amount</span>,
+			selector: row => row.total_amount,
+			sortable: true,
+			width: '175px'
+		}
+		// {
+		//   name: <span className='database-header'>Option</span>,
+		//   selector: (row) => {
+		//     return (
+		//       <>
+		//         <BsArrowUpRightSquare
+		//         size={20}
+		//         className="icon-btn-outline"
+		//         onClick={() => history.push(`/editcustomer/${row.id}`)}/>
+		//       </>
+		//     )
+		//   }
+		// }
+	];
+	return columns;
+};
--- src/utilities/table.utility.js
+++ src/utilities/table.utility.js
@@ -1,37 +1,45 @@
-
-
 export const autocomplete = (dataSource, text, columnName) => {
-    const textString = text;
+	const textString = text;
 
-    const filterResult = dataSource.filter((result, index) => {
+	const filterResult = dataSource.filter((result, index) => {
+		let filterText = result[columnName] ? result[columnName] : '';
 
-        let filterText = result[columnName] ? result[columnName] : '';
+		if (Number(result[columnName])) {
+			filterText = result[columnName].toString();
+			if (filterText.includes(textString)) {
+				return result;
+			}
+		}
 
-        if(Number(result[columnName])) {
-            filterText = result[columnName].toString();
-            if(filterText.includes(textString)) {
-                return result;
-            }
-        }
+		if (filterText.toLowerCase().includes(textString.toLowerCase())) {
+			return result;
+		}
+	});
 
-        if(filterText.toLowerCase().includes(textString.toLowerCase())) {
-            return result;
-        }
+	return filterResult;
+};
 
-    });
-
-    return filterResult;
-    
-
-}
+export const searchAll = (dataSource, text) => {
+	const textString = text;
+	const result = dataSource.filter(data => {
+		let filterText = data.code.toString();
+		let filterName = data.eng_name ? data.eng_name.toLowerCase() : '';
+		if (filterText.includes(textString)) {
+			return data;
+		} else if (filterName.includes(textString)) {
+			return data;
+		}
+	});
+	return result;
+};
 
 export const calculatePercentageAmount = (dataSource, percentageValue) => {
-    let resultTable = [];
-    resultTable = dataSource;
-     const result = resultTable.map(e => e.percentage = Number(e.percentage) + Number(percentageValue));
-        const requestBody = {
-            percentage: result.map(e => e)
-        }
-        // Api Update For All percentage.
-    return result;
-}
+	let resultTable = [];
+	resultTable = dataSource;
+	const result = resultTable.map(e => (e.percentage = Number(e.percentage) + Number(percentageValue)));
+	const requestBody = {
+		percentage: result.map(e => e)
+	};
+	// Api Update For All percentage.
+	return result;
+};
